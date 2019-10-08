1f5b7de3612c45bc3a3be1b5ea9e0b762153b8f8
Sverde-dot <55793525+Sverde-dot@users.noreply.github.com>
2019-10-08 08:58:46 +0200

    Add files via upload

---
+++ ejercicio2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EJ2;
+
+/**
+ *
+ * @author sverdecadilla
+ */
+public class ejercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void moin(String[] args) {
+        int base=3,altura=3,area;
+        area=(base*altura);
+        System.out.println("La base de un cuadrado de base "+ base+ " y altura  " + altura+ " es igual a " + area);
+    }
+    
+}
